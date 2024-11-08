f4673a91a24bd5465d55cb04aa7ec96e32d832a0
Martialdepaul <bourangoneulomimartialdepaul@gmail.com>
2024-11-08 14:42:32 +0100

    Initial commit

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+
+export default function UserInput({ onChange, userInput }) {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="initial-investment">Initial Investment</label>
+          <input
+            id="initial-investment"
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) => onChange("initialInvestment", event.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="annual-investment">Annual Investment</label>
+          <input
+            id="annual-investment"
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) => onChange("annualInvestment", event.target.value)}
+          />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">Expected Return</label>
+          <input
+            id="expected-return"
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) => onChange("expectedReturn", event.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Duration</label>
+          <input
+            id="duration"
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => onChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
+
+UserInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  userInput: PropTypes.shape({
+    initialInvestment: PropTypes.number.isRequired,
+    annualInvestment: PropTypes.number.isRequired,
+    expectedReturn: PropTypes.number.isRequired,
+    duration: PropTypes.number.isRequired,
+  }).isRequired,
+};
